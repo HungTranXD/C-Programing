19b51ea9412c74cc6edb98e66306e7e30c5e2dcc
HungTranXD <hungtranxd2697@gmail.com>
2023-01-03 11:42:24 +0700

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1.session1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int x = 10;
+        double y = 3.14;
+        Console.WriteLine("Hello world: " + x);
+        string s = "T2204M";
+
+        int[] ns = new int[3];
+        ns[0] = 15;
+        ns[1] = 20;
+        ns[2] = 25;
+
+        foreach(int n in ns)
+        {
+            // n
+        }
+
+        for(int i = 0; i < ns.Length; i++)
+        {
+            // n[i]
+        }
+
+        //INPUT AND OUTPUT
+        string input = Console.ReadLine(); // scan
+        int num = Convert.ToInt32(input);
+        Console.WriteLine("num = " + num);
+
+        //CLASS AND OBJECT
+        Human h = new Human();
+        h.name = "Nguyen Van An";
+        h.Run();
+    }
+}
---
+++ ConsoleApp1/session1/Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; // giong import
+
+namespace ConsoleApp1.session1 //giong package trong Java nhung namespace khong lien quan den folder (tu dat ten cung duoc, sau nay cung namespace thi duoc tong hop trong dll)
+{
+    internal class Human
+    {
+        public string name;
+        public int age;
+
+        public void Eat() //ham trong C# thuong viet hoa chu dau
+        {
+            Console.WriteLine("Eating...");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(this.name + " running...");
+        }
+
+    }
+}
